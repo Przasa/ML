55c34a0e9846c481e24c0863eadcf7bec18fd5ff
Przasa <rzasapiotrek@gmail.com>
2021-04-26 08:32:09 +0200

    zaczynamy z polynomialsami

--- 4_learning_models.py
+++ 4_learning_models.py
@@ -40,7 +40,7 @@ def calc_theta_linreg(X,y):
     # yp_linreg=lin_reg.predict(X_vec)
     return theta_linreg #,yp_linreg
 
-def calc_theta_svd(X,y):
+def calc_theta_svd(X,y): #SVD = Singular Value Decompositino (do wyliczania macierzy pseudoodwrotnej)
     X_vec=np.c_[np.ones((len(X),1)),X]
     theta_svd ,_esiduals, _rank, _s =np.linalg.lstsq(X_vec, y, rcond=1e-6)
     return theta_svd
@@ -109,8 +109,14 @@ def makeAnalyze():
         aprinter.addTheta(x)
     aprinter.compare_thetas()
 
-print("code prepared")
 
+def closedForms(X,y):
+    theta_cform, theta_linreg, theta_svd = calc_theta_corm(X,y),calc_theta_linreg(X,y),calc_theta_svd(X,y)
+    yp_cform,yp_linreg,yp_svd = predict_by_theta(Xs,theta_cform),predict_by_theta(Xs,theta_linreg),predict_by_theta(Xs,theta_svd)
+    
+    makeAnalyze()       #troche przerost formy z ta funckja
+
+#TODO: zrob tabliczke z uzyskanyi wynikami gradientow i cech (dla wszystkich)
 def batchGradientDescent(X,y):
 #wsadowy gradient prosty 
 # -> trzeba od razu wszystkie dane, ale niezly dla wielu cech (liczy je jednoczesnie)
@@ -127,7 +133,7 @@ def batchGradientDescent(X,y):
     glist=[]
     tlist=[]
     for iterations in range(n_iterations):
-        gradients = 2/m + X_vec.T.dot(X_vec.dot(theta)-y)
+        gradients = 2/m + X_vec.T.dot(X_vec.dot(theta)-y)   #za kazdym razem, liczymu gradient dla wszystkich sampli
         theta = theta - eta*gradients
         glist.append(gradients.T[0])
         tlist.append(theta.T[0])
@@ -138,17 +144,108 @@ def batchGradientDescent(X,y):
 
     plt.figure(1)
     plt.plot(tarr[:,0],tarr[:,1],'b')
+    plt.xlabel('$x_0$')
+    plt.ylabel("$x_1$")
     plt.grid(True)
 
     plt.figure(2)
     plt.plot(garr[:,0],garr[:,1],'b')
+    plt.xlabel("gradient($x_0$)")
+    plt.ylabel("graident($x_1$)")
+    plt.grid(True)
+    
+def stochastidGradientDescent(X,y):
+    X_vec=np.c_[np.ones((len(X),1)),X]
+    eta=0.007
+    n_epochs=50
+    t0, t1= 5,50
+    # m=samples_qty
+    m=len(X_vec)
+
+    def learning_schedule(t):
+        return t0/(t+t1)
+
+    theta = np.random.randn(2,1)
+
+    glist=[]
+    tlist=[]
+    for epoch in range(n_epochs):
+        for i in range(int(m)):          #uwaga, sample sa losowane (nie trzeba liczyc wszystkich na raz). moglibysmy losowa mniejsza ilosc razy
+            random_index = np.random.randint(m)
+            xi = X_vec[random_index:random_index+1]     
+            yi= y[random_index:random_index+1]
+            gradients = 2 * xi.T.dot(xi.dot(theta)-yi)  #liczymy gradient dla jednego losowego sampla i mozemy przerwac liczenie w dowolnym momencie
+            eta = learning_schedule(epoch*m +i)             #eta_min = 5/(50+50*100)= 
+            theta=theta-eta * gradients
+            glist.append([gradients[0][0],gradients[1][0]])
+            tlist.append([theta[0][0],theta[1][0]])
+    garr=np.asarray(glist)
+    tarr=np.asarray(tlist)
+
+
+    plt.figure(3)
+    plt.plot(tarr[:,0],tarr[:,1],'b')
+    plt.xlabel('$x_0$')
+    plt.ylabel("$x_1$")
+    plt.grid(True)
+    plt.show()
+
+    plt.figure(4)
+    plt.plot(garr[:,0],garr[:,1],'b')
+    plt.xlabel("gradient($x_0$)")
+    plt.ylabel("graident($x_1$)")
     plt.grid(True)
 
-def closedForms(X,y):
-    theta_cform, theta_linreg, theta_svd = calc_theta_corm(X,y),calc_theta_linreg(X,y),calc_theta_svd(X,y)
-    yp_cform,yp_linreg,yp_svd = predict_by_theta(Xs,theta_cform),predict_by_theta(Xs,theta_linreg),predict_by_theta(Xs,theta_svd)
-    
-    makeAnalyze()       #troche przerost formy z ta funckja
+
+print("code prepared")
+
+def minibatchGradientDescent(X,y):
+    theta_path_mgd = []
+    grad_path_mgd = []
+
+    n_iterations = 50
+    minibatch_size = 20
+    m=len(X)
+    X_vec=np.c_[np.ones((len(X),1)),X]
+
+    np.random.seed(42)
+    theta = np.random.randn(2,1)  # inicjalizacja losowa
+
+    t0, t1 = 200, 1000
+    def learning_schedule(t):
+        return t0 / (t + t1)
+
+    t = 0
+    for epoch in range(n_iterations):
+        shuffled_indices = np.random.permutation(m)
+        X_vec_shuffled = X_vec[shuffled_indices]
+        y_shuffled = y[shuffled_indices]
+        for i in range(0, m, minibatch_size):
+            t += 1
+            xi = X_vec_shuffled[i:i+minibatch_size]
+            yi = y_shuffled[i:i+minibatch_size]
+            gradients = 2/minibatch_size * xi.T.dot(xi.dot(theta) - yi)
+            eta = learning_schedule(t)
+            theta = theta - eta * gradients
+            theta_path_mgd.append(theta)
+            grad_path_mgd.append(gradients)
+
+    theta_path_mgd = np.asarray(theta_path_mgd)
+    grad_path_mgd = np.asarray(grad_path_mgd)
+
+    plt.figure(5)
+    plt.plot(theta_path_mgd[:,0],theta_path_mgd[:,1],'b')
+    plt.xlabel('$x_0$')
+    plt.ylabel("$x_1$")
+    plt.grid(True)
+    plt.show()
+
+    plt.figure(6)
+    plt.plot(grad_path_mgd[:,0],grad_path_mgd[:,1],'b')
+    plt.xlabel("gradient($x_0$)")
+    plt.ylabel("graident($x_1$)")
+    plt.grid(True)
+
 
 
 # %%
@@ -159,37 +256,53 @@ if('X' not in globals() or 'y' not in globals() or FORCE_CALC):
     Xs=np.c_[np.asarray(range(0,20+1,1))/10]
 
 
+# TODO: troche otworz ta funkcje
 # closedForms(X,y)
 
+#TODO: zrobi jakies porownanie szybkosci tych gradientow
 # batchGradientDescent(X,y)
-
 # stochastidGradientDescent(X,y)
-X_vec=np.c_[np.ones((len(X),1)),X]
-eta=0.007
-n_epochs=500
-t0, t1= 5,50
-m=samples_qty
+# minibatchGradientDescent(X,y)
+#TODO: mozne wrzucic jeszcze uczenie przez  SGD Regressor
 
-
-def learning_schedule(t):
-    return t0/(t+t1)
-
-theta = np.random.randn(2,1)
-
-for epoch in range(n_epochs):
-    for i in range(int(m/10)):          #uwaga, sample sa losowane (nie trzeba liczyc wszystkich na raz). moglibysmy losowa mniejsza ilosc razy
-        random_index = np.random.randint(m)
-        xi = X_vec[random_index:random_index+1]     
-        yi= y[random_index:random_index+1]
-        gradients = 2 * xi.T.dot(xi.dot(theta)-yi)
-        eta = learning_schedule(epoch*m +i)             #eta_min = 5/(50+50*100)= 
-        theta=theta-eta * gradients
-
-print(theta)
-print('kot)')
+del(X)
+del(y)
 
 
 
 
-#%%
-for t in tlist:
+#%% Polynomial regression
+from sklearn.preprocessing import PolynomialFeatures
+
+if('X' not in globals() or 'y' not in globals() or FORCE_CALC):
+    n_samples =100
+    X = 6 * np.random.rand(n_samples,1)-3
+    y= 0.5 * X**2  +2 + np.random.randn(n_samples,1)
+
+
+poly = PolynomialFeatures(degree=2,include_bias=False)
+X_poly=poly.fit_transform(X)        # nie musielismy wrzucac X0 = 1
+lin_reg = LinearRegression()
+lin_reg.fit(X_poly,y)
+
+theta = [x for x  in lin_reg.coef_[0]]
+theta.insert(0,lin_reg.intercept_)
+X_poly_voc = np.c_[np.ones((len(X),1)),X_poly]
+yp=X_poly_voc.dot(theta)
+yp.resize((len(yp),1))
+
+plt.figure(7)
+plt.plot(X, y, "r.")
+plt.plot(X, yp, "b.")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.axis([-3, 3, 0, 10])
+plt.title("Polynomial regression")
+# save_fig("r_4_12")
+plt.show()
+
+#razczej tak rob
+# X_new=np.linspace(-3, 3, 100).reshape(100, 1)
+# X_new_poly = poly_features.transform(X_new)
+# y_new = lin_reg.predict(X_new_poly)
+
